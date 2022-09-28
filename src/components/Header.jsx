0434aedb430e6c9e850d27312e99eca0293ad929
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 15:53:05 +0530

    Create navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
+import logo from '../assets/logo.png'
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
-    <header>
-      <p>Header</p>
+    <header className="flex justify-between">
+      <img className="w-56" src={logo} />
+      <nav>
+      <Link to="/">
+        <a href="/">Home</a>
+      </Link>
+      <Link to="/todolist">
+        <a href="/">TodoList</a>
+      </Link>
+      </nav>
     </header>
   )
 }
