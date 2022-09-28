6e6df90fe888da30f2b4882bc5ee839679c3a5e1
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 18:14:00 +0530

    Add base url

--- src/redux/todos/todos.js
+++ src/redux/todos/todos.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
-// actions
+// const
+const BASEURL ="http://localhost:8080/api/"
 const ADD_TODO = "todolist/todos/ADD_TODO";
 const REMOVE_TODO = "todolist/todos/REMOVE_TODO";
 const FETCH_TODO = "todolist/todos/FETCH_TODO";
@@ -23,6 +24,18 @@ const fetchTodo = (payload) => ({
   payload,
 });
 
+// api functions
+export const fetchBookApi = () => async (dispatch) => {
+  const books = await Axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ihHFtrkDGalHB4Kt6Ws8/books');
+  const mapBooks = Object.entries(books.data).map(([id, book]) => {
+    const { category, title } = book[0];
+    return { id, category, title };
+  });
+  dispatch(fetchTodo(mapBooks));
+};
+
+
+// recucer
 const todoReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
