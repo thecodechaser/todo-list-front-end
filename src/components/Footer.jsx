7b770ebd1a85680f10868b92378a5ff96559f82a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 16:17:11 +0530

    Create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>Im footer</p>
+    <footer className="bottom-0 absolute py-10 w-full">
+      <p className="text-center text-primaryColor text-lg">Made with ❤️ by <a className="" 
+      href="https://thecodechaser">thecodechaser</a> for <a href="https://www.cognitensor.com/">CogniTensor</a></p>
     </footer>
   )
   }
