5bc135f0e1dce62845d33d97df5fd303ceddb045
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 17:36:50 +0530

    Apply styles to components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,9 @@ const Header = () => {
 
   return (
     <header className="flex justify-between px-6 md:px-20 py-5 shadow-xl w-full">
-      <img className="w-32 md:w-56" src={logo} />
+     <Link to="/"><img className="w-32 md:w-56" src={logo} /></Link> 
       <nav>
-        <ul className="flex gap-2">
+        <ul className="flex gap-4">
           {menu.map((item) => (
             <li key={item.id}>
               <Link to={item.path}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import Welcome from "../components/Home/Welcome";
+import { Link } from "react-router-dom";
+import { FaRegCalendarCheck } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className="flex justify-center mt-32">
+    <div className="flex justify-center mt-5 lg:mt-32">
       <div className="flex flex-col shadow-md m-5 md:w-1/2 shadow-primaryColor p-10 gap-5">
         <Welcome/>
+        <div className="flex justify-center  mt-10">
+
+        
+        <Link to="/todolist">
+                <a
+                  className="text-lg font-semibold text-primaryColor border-2 p-2 rounded border-primaryColor"
+                  href="/todolist"
+                >
+                  Goto TodoList< FaRegCalendarCheck className="cursor-pointer inline ml-2 mb-1"/>
+                </a>
+              </Link>
+              </div>
       </div>
     </div>
   );
