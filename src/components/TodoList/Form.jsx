6c7f849bdfe4a4b3beee70b248e045fdd9adf852
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 17:51:39 +0530

    Apply styles to form

--- src/components/TodoList/Form.jsx
+++ src/components/TodoList/Form.jsx
@@ -1,16 +1,19 @@
+import { FaRegEdit } from "react-icons/fa";
+
 const Form = () => {
   return (
-    <div>
+    <div className="mb-5">
        <input
         required
-        className="w-64 lg:w-72"
+        className="w-64 lg:w-80 border-2 rounded border-primaryColor focus:outline-none"
         type="text"
         placeholder="Task name..."
         // value={title}
         // onChange={(e) => setTask(e.target.value)}
         maxLength="20"
       />
-      <button className="ml-8 lg:ml-12 text-primaryColor font-semibold" type="submit">Add Task</button>
+      <button className="ml-8 lg:ml-2 text-primaryColor font-semibold" type="submit">
+        Add Task< FaRegEdit className="cursor-pointer inline ml-2 mb-1"/></button>
     </div>
   )
 }
