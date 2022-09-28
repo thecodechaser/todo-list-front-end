69c8978818a0f890c7e9c9c0d7c595eec0b8ea06
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 15:43:43 +0530

    Create header comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import TodoList from "./pages/TodoList";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todolist" element={<TodoList />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <p>Header</p>
+    </header>
+  )
+}
+
+export default Header;
