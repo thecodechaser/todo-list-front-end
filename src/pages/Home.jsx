2cfef596f6e6ec582c8a20789b9d2e9bc5a4931b
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 17:28:28 +0530

    Add welcome message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import Welcome from "../components/Home/Welcome";
+
 const Home = () => {
-  return <p>Home Page</p>;
+  return (
+    <div className="flex justify-center mt-32">
+      <div className="flex flex-col shadow-md m-5 md:w-1/2 shadow-primaryColor p-10 gap-5">
+        <Welcome/>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
