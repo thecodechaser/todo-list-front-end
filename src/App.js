1174e813c344082eb9179a77bcd1dbd633e7cd43
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 16:07:19 +0530

    Import Footer in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import TodoList from "./pages/TodoList";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/todolist" element={<TodoList />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-const Footer = () => (
+const Footer = () => {
   return (
     <footer>
       <p>Im footer</p>
     </footer>
   )
-)
+  }
 
 export default Footer;
