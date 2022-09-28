390f3126a674c5ea0faf11a2a69b2067c3f78834
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 13:25:59 +0530

    Fix issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello</p>
+        <p className="text-5xl">Hello</p>
       </header>
     </div>
   );
