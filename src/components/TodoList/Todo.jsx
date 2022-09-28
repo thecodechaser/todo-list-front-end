3fa40b30eb55780c57e9ccc8d25ac675c157a16a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 17:05:07 +0530

    Add markup for todos

---
+++ src/components/TodoList/Todo.jsx
@@ -0,0 +1,7 @@
+const Todo = () => {
+  return (
+    <p>This is a todo task gffffffffffffffuys</p>
+  )
+};
+
+export default Todo;
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,5 +1,18 @@
+import Todo from "../components/TodoList/Todo";
+
+const data =[1,2,3,4,5]
 const TodoList = () => {
-  return <p>Todo List</p>;
+  return (
+    <div className="flex justify-center mt-32">
+      <div className="flex flex-col shadow-md md:w-1/3 shadow-primaryColor p-10 gap-5">
+        {
+          data.map(()=>(
+            <Todo />
+          ))
+        }
+      </div>
+    </div>
+  );
 };
 
 export default TodoList;
