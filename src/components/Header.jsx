d6cb715f67d32e39fbe6c562e3fa59adb9eeaaa6
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 16:04:34 +0530

    Add styles to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   {id: 1,name: "TodoList", path: "/todolist"}]
 
   return (
-    <header className="flex justify-between px-20 py-5">
+    <header className="flex justify-between px-20 py-5 shadow-xl w-full">
       <img className="w-56" src={logo} />
       <nav>
         <ul className="flex gap-2">
@@ -15,7 +15,7 @@ const Header = () => {
         menu.map((item) =>(
           <li key={item.id}>
           <Link  to={item.path}>
-        <a href={item.path}>{item.name}</a>
+        <a className="text-lg font-semibold text-primaryColor" href={item.path}>{item.name}</a>
       </Link>
       </li>
         ))
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primaryColor: '#9547F7',
+      secondaryColor: '#06C1FF',
+      skyColor: '#addbea',
+      darkSkyColor: '#608297',
+      blueColor: '#013F70',
+      lightBlueColor: '#118AB2',
+    },
   },
   plugins: [],
 };
