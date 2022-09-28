7a2b72695738f4548718ee305ee98abce65bfaae
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 17:46:18 +0530

    Add markup to form

--- src/components/TodoList/Form.jsx
+++ src/components/TodoList/Form.jsx
@@ -1,6 +1,17 @@
 const Form = () => {
   return (
-    <p>Form</p>
+    <div>
+       <input
+        required
+        className="w-64 lg:w-72"
+        type="text"
+        placeholder="Task name..."
+        // value={title}
+        // onChange={(e) => setTask(e.target.value)}
+        maxLength="20"
+      />
+      <button className="ml-8 lg:ml-12 text-primaryColor font-semibold" type="submit">Add Task</button>
+    </div>
   )
 }
 
