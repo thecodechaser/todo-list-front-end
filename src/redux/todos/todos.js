b06e4c75d3910a3bf356ebc64a5e5682f5e27cba
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 14:28:24 +0530

    Add reducer

--- src/redux/todos/todos.js
+++ src/redux/todos/todos.js
@@ -1,2 +1,15 @@
 // initial-state
 const initialState = [];
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    case FETCH_BOOK:
+      return action.payload;
+    default:
+      return state;
+  }
+};
