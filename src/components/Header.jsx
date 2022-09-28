da4ef00eefdfb381da6085aff986e06bdcc908cf
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 15:59:29 +0530

    Create nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,25 @@ import logo from '../assets/logo.png'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+  const menu = [{id: 1,name: "Home", path: "/"},
+  {id: 1,name: "TodoList", path: "/todolist"}]
+
   return (
-    <header className="flex justify-between">
+    <header className="flex justify-between px-20 py-5">
       <img className="w-56" src={logo} />
       <nav>
-      <Link to="/">
-        <a href="/">Home</a>
+        <ul className="flex gap-2">
+      {
+        menu.map((item) =>(
+          <li key={item.id}>
+          <Link  to={item.path}>
+        <a href={item.path}>{item.name}</a>
       </Link>
-      <Link to="/todolist">
-        <a href="/">TodoList</a>
-      </Link>
+      </li>
+        ))
+      }
+      </ul>
       </nav>
     </header>
   )
