744364da77de36d9e4d95e0cf95ac40db3861751
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 16:05:15 +0530

    Fix format

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,33 @@
-import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom';
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-
-  const menu = [{id: 1,name: "Home", path: "/"},
-  {id: 1,name: "TodoList", path: "/todolist"}]
+  const menu = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 1, name: "TodoList", path: "/todolist" },
+  ];
 
   return (
     <header className="flex justify-between px-20 py-5 shadow-xl w-full">
       <img className="w-56" src={logo} />
       <nav>
         <ul className="flex gap-2">
-      {
-        menu.map((item) =>(
-          <li key={item.id}>
-          <Link  to={item.path}>
-        <a className="text-lg font-semibold text-primaryColor" href={item.path}>{item.name}</a>
-      </Link>
-      </li>
-        ))
-      }
-      </ul>
+          {menu.map((item) => (
+            <li key={item.id}>
+              <Link to={item.path}>
+                <a
+                  className="text-lg font-semibold text-primaryColor"
+                  href={item.path}
+                >
+                  {item.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      primaryColor: '#9547F7',
-      secondaryColor: '#06C1FF',
-      skyColor: '#addbea',
-      darkSkyColor: '#608297',
-      blueColor: '#013F70',
-      lightBlueColor: '#118AB2',
+      primaryColor: "#9547F7",
+      secondaryColor: "#06C1FF",
+      skyColor: "#addbea",
+      darkSkyColor: "#608297",
+      blueColor: "#013F70",
+      lightBlueColor: "#118AB2",
     },
   },
   plugins: [],
