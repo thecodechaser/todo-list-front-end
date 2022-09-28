f1ca3cc125130bbb2803ee888a40fe02979ef660
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 14:35:08 +0530

    Configure store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import todoReducer from './todos/todos';
+
+const reducer = combineReducers({
+  todoReducer,
+});
+
+const store = createStore(
+  reducer, applyMiddleware(logger, thunk),
+);
+
+export default store;
