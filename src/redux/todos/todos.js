d11c4d0c939996fdeef24b31c062b79381563779
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-09-28 14:33:59 +0530

    Add actions

--- src/redux/todos/todos.js
+++ src/redux/todos/todos.js
@@ -1,18 +1,34 @@
 // actions
-const ADD_BOOK = 'bookStore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-const FETCH_BOOK = 'bookStore/books/FETCH_BOOK';
+const ADD_TODO = 'todolist/todos/ADD_TODO';
+const REMOVE_TODO = 'todolist/todos/REMOVE_TODO';
+const FETCH_TODO = 'todolist/todos/FETCH_TODO';
 
 // initial-state
 const initialState = [];
 
+// actions
+const addTodo = (payload) => ({
+  type: ADD_TODO,
+  payload,
+});
+
+const removeTodo = (payload) => ({
+  type: REMOVE_TODO,
+  payload,
+});
+
+const fetchTodo = (payload) => ({
+  type: FETCH_TODO,
+  payload,
+});
+
 const todoReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_BOOK:
+    case ADD_TODO:
       return [...state, action.payload];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
-    case FETCH_BOOK:
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.payload);
+    case FETCH_TODO:
       return action.payload;
     default:
       return state;
